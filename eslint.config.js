5a503e08a52cbbbcc8c547847e3236b21fa58613
nexryai <nexryai@sda1.net>
2024-06-25 16:11:48 +0900

    Add files

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        rules: {
+            "no-unused-vars": "error",
+            "no-undef": "error",
+            "indent": ["error", 4],
+            "quotes": ["error", "double"],
+            "semi": ["error", "never"],
+        }
+    }
+];
