11597c27067234001221175109542ff76a508b00
nexryai <nexryai@sda1.net>
2024-07-03 08:33:19 +0900

    Client: ボタンで移動するとアニメーションが効かなくなるので修正

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,9 +1,5 @@
 <script>
     import { Button } from "$lib/components/ui/button";
-
-    function moveToLoginPage() {
-        location.href = "/signin"
-    }
 </script>
 
 <svelte:head>
@@ -55,7 +51,7 @@
 	</div>
 
     <div class="start-actions">
-        <Button on:click={moveToLoginPage}>
+        <Button href="/signin">
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-login"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" /><path d="M21 12h-13l3 -3" /><path d="M11 15l-3 -3" /></svg>
             　Login and continue
         </Button>
