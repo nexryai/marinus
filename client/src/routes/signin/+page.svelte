8535b985f473634049b44763cd067aa071385957
nexryai <nexryai@sda1.net>
2024-07-02 20:35:34 +0900

    Client: Remove Microsoft login and add loginWithGitHub()

--- client/src/routes/signin/+page.svelte
+++ client/src/routes/signin/+page.svelte
@@ -9,6 +9,10 @@
     function loginWithGoogle() {
         location.href = "/login/google"
     }
+
+    function loginWithGitHub() {
+        location.href = "/login/github"
+    }
 </script>
 
 <div class="text-column">
@@ -30,14 +34,9 @@
             　Sign in with Google
         </Button>
 
-        <Button>
+        <Button on:click={loginWithGitHub}>
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg>
             　Sign in with GitHub
-        </Button>
-
-        <Button>
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-azure"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 7.5l-4 9.5h4l6 -15z" /><path d="M22 20l-7 -15l-3 7l4 5l-8 3z" /></svg>
-            　Sign in with Microsoft
         </Button>
     </div>
 </div>
