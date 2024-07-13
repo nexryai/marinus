880754e04e434893f4c53ac1b61664ff49f0579b
nexryai <nexryai@sda1.net>
2024-07-13 09:32:35 +0900

    Refactor startup code

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "@/app.module.js"
+import { logError, logInfo } from "@/utils/log.js"
 
 async function bootstrap() {
     const app = await NestFactory.createApplicationContext(AppModule)
@@ -7,5 +8,21 @@ async function bootstrap() {
     main.startServer(8080)
 }
 
-process.title = "NewsBoard Server"
-bootstrap()
+async function boot() {
+    process.title = "NewsBoard Server"
+
+    try {
+        bootstrap().then(r => logInfo("Server started!"))
+    } catch (e) {
+        if (e instanceof Error) {
+            logError(`Failed to start server!: ${e.message}`)
+            console.error(e)
+        }
+        process.exit(1)
+    }
+}
+
+boot().catch(e => {
+    console.error(e)
+    process.exit(1)
+})
