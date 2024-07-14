9b584212bae8317a7fe7e4ff840efcfcaee95596
nexryai <nexryai@sda1.net>
2024-07-14 14:27:24 +0900

    Client: WIP

--- client/src/lib/components/timeline/timeline.svelte
+++ client/src/lib/components/timeline/timeline.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import { AddFeedButton } from "$lib/components/addFeedButton"
+    import DiscoverCard from "$lib/components/core/discoverCard.svelte"
+
+    function isNoteEmpty() {
+        return true
+    }
 </script>
 
 <div class="timeline">
@@ -7,6 +12,13 @@
         <h2>No note!</h2>
         <p>Click the button below to subscribe new feed.</p>
     </div>
+    <DiscoverCard
+        url="https://www.sda1.net"
+        title="Welcome to NewsBoard!"
+        description="Simple and Open-Source modern RSS reader"
+        feedSource="nexryai"
+        imageUrl="https://s3.sda1.net/nnm/contents/b6bcec6c-cb93-4e88-a6d0-46e41260ae20.png"
+    />
     <div class="floating-button">
         <AddFeedButton />
     </div>
