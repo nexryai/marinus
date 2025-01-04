5598aa7db97f69798f1dbe9865f7398e9a670165
nexryai <nexryai@sda1.net>
2025-01-04 20:03:21 +0900

    Feat: using vitest for unit tests

--- eslint.config.js
+++ eslint.config.js
@@ -46,7 +46,7 @@ export default ts.config(
                     groups: ["builtin", "external", "parent", "sibling", "index", "object"],
                     pathGroups: [
                         {
-                            pattern: "{svelte,$app/**,elysia}",
+                            pattern: "{svelte,$app/**,elysia,vitest}",
                             group: "builtin",
                             position: "before",
                         },
