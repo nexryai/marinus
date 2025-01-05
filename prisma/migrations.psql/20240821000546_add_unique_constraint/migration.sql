cdf193a39f426166db172e69429cfca1c8d1aecd
nexryai <nexryai@sda1.net>
2025-01-05 10:51:57 +0900

    Clean up

--- prisma/migrations.psql/20240821000546_add_unique_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,feedId]` on the table `Subscription` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Subscription_userId_feedId_key" ON "Subscription"("userId", "feedId");
--- src/lib/server/prisma.repository.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export class PrismaService extends PrismaClient {
-    async onModuleInit() {
-        await this.$connect();
-    }
-}
