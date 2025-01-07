fd2ffbb371a34dfa9bdbf251b48d443b56e64ff6
nexryai <nexryai@sda1.net>
2025-01-07 17:27:34 +0900

    フォントの読み込みを高速化

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -36,11 +36,6 @@
 <svelte:head>
     <title>Ablaze Marinus</title>
     <meta name="description" content="Modern and simple RSS reader" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="preconnect" href="https://cdn.jsdelivr.net">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Noto+Sans+KR:wght@100..900&family=Noto+Sans+SC:wght@100..900&family=Outfit:wght@100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Latin/MiSansLatin-Medium.min.css">
 </svelte:head>
 
 <div class="app">
