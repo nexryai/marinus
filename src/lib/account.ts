dc0ecabd225e8022b5a191b9e9b7292651aa17e4
nexryai <nexryai@sda1.net>
2025-01-07 09:03:21 +0900

    Remove console.log

--- src/lib/account.ts
+++ src/lib/account.ts
@@ -6,6 +6,6 @@ export function isLoggedIn() {
         console.error("This function is only available in the browser");
         return false;
     }
-    console.log(document.cookie);
+
     return document.cookie.includes("isLoggedIn=true");
 }
