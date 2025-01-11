71dafd3cda98173b1cb915a0dcfa0ca9f879331a
nexryai <nexryai@sda1.net>
2025-01-11 13:57:18 +0900

    Add /about & /terms

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,19 +3,48 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>About this app</h1>
+<p class="text-xl">About this app</p>
+<div class="mx-6">
+    <div class="mt-16">
+        <p class="text-2xl font-outfit">Ablaze Marinus</p>
+        <p>©2024-2025 Ablaze / nexryai  All rights reserved.</p>
+        <div>
+            <p class="mt-6 font-extrabold">
+                As far as the law allows, the software comes as is, without any warranty or
+                condition, and the licensor will not be liable to you for any damages arising
+                out of these terms or the use or nature of the software, under any kind of
+                legal claim.
+            </p>
+            <p class="mt-2 font-bold">
+                法律が許す限り、ソフトウェアは現状のまま、いかなる保証や条件もなく提供されます。
+                ライセンサーは、いかなる法的請求においても、本規約またはソフトウェアの使用または性質から生じるいかなる損害についても責任を負いません。
+            </p>
+        </div>
+    </div>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+	<div class="mt-16">
+        <div class="mt-12">
+            <p>This software uses <a class="text-gray-600" href="https://hyperos.mi.com/font/download">Xiaomi MiSans font.</a> <br></p>
+            <div class="ml-4 mt-4">
+                <p>Copyright © Beijing Xiaomi Mobile Software Co.,Ltd. All Rights Reserved.</p>
+                <p class="mt-2">本《MiSans 字体知识产权许可协议》（以下简称“协议”）是您与小米科技有限责任公司（以下简称“小米”或“许可方”）之间有关安装、使> 用 MiSans 字体（以下简称“MiSans”或“MiSans 字体”）的法律协议。您在使用 MiSans 的所有或任何部分前，应接受本协议中规定的> 所有条款和条件。安装、使用 MiSans 的行为表示您同意接受本协议所有条款的约束。否则，请不要安装或使用 MiSans，并应立即销毁和> 删除所有 MiSans 字体包。 </p>
+                <p class="mt-2">根据本协议的条款和条件，许可方在此授予您一份不可转让的、非独占的、免版税的、可撤销的、全球性的版权许可，使您依照本协议约定使> 用 MiSans 字体，前提是符合下列条件：</p>
+                <ul class="mt-2 ml-8 list-disc">
+                    <li>您应在软件中特别注明使用了 MiSans 字体。</li>
+                    <li>您不得对 MiSans 字体或其任何单独组件进行改编或二次开发。</li>
+                    <li>您不得单独将 MiSans 字体或其组件对外租赁、再许可、给予、出借或进一步分发字体软件或其任何副本以及重新分发或售卖。此限制不适用于您使用 MiSans 字体创作的任何其他作品。如您使用 MiSans 字体创作宣传素材、logo、应用 App 等，您有权分发或出售该作品。</li>
+                </ul>
+            </div>
+        </div>
+        <div class="mt-12 mb-12">
+            <span>Made with</span>
+            <img width="300px" height="auto" src="https://raw.githubusercontent.com/sveltejs/branding/ff063f89d1cc399b110c574ab6fe0b29cef5d617/svelte-kit-horizontal.svg" alt="SvelteKit Logo">
+        </div>
+	</div>
 </div>
+
+<style>
+    .font-outfit {
+        font-family: "Outfit", sans-serif;
+    }
+</style>
---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,114 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="mx-6">
+    <div class="mt-12">
+        <p class="text-2xl font-outfit">Ablaze Marinus Terms and Privacy Policy</p>
+        <p class="mt-4">
+            <span>Please read carefully. By using this service, you are deemed to have read and fully agreed to all the terms of this agreement.</span><br>
+            以下、注意深くお読みください。このサービスを利用することにより、この規約の全ての条項の文章を理解し、完全に同意したものとします。
+        </p>
+        <div class="mt-10">
+            <p class="text-lg">Ⅰ. ソフトウェアに関する免責事項</p>
+            <p class="mt-2">
+                法律が許す限り、このサービスによって提供される全てのソフトウェアは<span class="font-bold">現状のまま、いかなる保証や条件もなく</span>提供されます。
+                ライセンサーは、いかなる法的請求においても、本規約またはソフトウェアの使用または性質から生じるいかなる損害についても責任を負いません。
+            </p>
+
+            <p class="text-lg mt-8">Ⅱ. サービスに関する免責事項</p>
+            <p class="mt-2">
+                <span class="font-bold">本サービスに関する可用性、正確性、信頼性、完全性、適時性、およびその他のいかなる保証も、明示的または黙示的であるかを問わず、一切提供されません。</span>
+                本サービスを利用したことによって発生したいかなる損害もAblaze、及び本ソフトウェアのライセンサーは一切の責任を負わないものとします。
+                本サービスの運営者は、一切の予告なしに本サービスの提供を停止、変更、または終了する権利、及び任意のユーザーのアカウントを削除、凍結、制限する権利を留保します。
+                <span class="font-bold">本サービスを利用することで、本サービスに関しての訴訟を起こす権利（集団訴訟、及びそれに参加する権利を含む）、調停や仲裁を含む裁判官や陪審員に審理してもらう権利を永久に放棄したことになります。</span>
+            </p>
+            <p class="mt-2">
+                <span class="text-red-700 font-extrabold">This service complies with laws and regulations in Japan and does not adhere to the GDPR (General Data Protection Regulation) of the European Union or the CCPA (California Consumer Privacy Act) of the United States. By using this service, you agree to waive any rights based on these laws.</span><br>
+                本サービスは日本国内の法令・規制に準拠しており、 EUのGDPR（一般データ保護規則） や米国カリフォルニア州のCCPAに準拠していません。本サービスを利用することで、これらの法律に基づく権利を放棄したことになります。<br>
+            </p>
+
+            <p class="text-lg mt-8">Ⅲ. 利用にあたっての制限</p>
+            <p class="mt-2">
+                本サービスの利用者が、本サービスの利用にあたって以下の行為を行うことを禁止します。
+            </p>
+
+            <ul class="mt-2 ml-8 list-disc">
+                <li>システムに対する不正アクセス、不正利用、及び意図的に可用性を著しく低下させる行為</li>
+                <li>システムで設定された制限を回避する行為</li>
+                <li>他のユーザーに対する迷惑行為</li>
+            </ul>
+            <div class="mt-4">
+                いかなる形においても、本サービスを利用して以下に該当するコンテンツの拡散、提供、及びそれに加担する行為を行うことを禁止します。<br>
+                またこのようなコンテンツの拡散に加担していると判断されたウェブサイトのドメインやURLには、フィードの購読を拒否するなどの制限が課せられることがあります。
+                <ul class="mt-2 ml-8 list-disc">
+                    <li>児童ポルノなど、日本国内で違法となるポルノコンテンツ</li>
+                    <li>陰謀論、悪質な誤情報</li>
+                    <ul class="mt-1 mb-2 ml-8 list-disc">
+                        <li>予防接種に関する虚偽の主張、科学的根拠のない治療方法の拡散など公衆衛生・医療に関連する悪質な誤情報</li>
+                        <li>新型コロナウイルス感染症（COVID-19）に関する誤情報</li>
+                        <li>選挙における立候補者の資格や投票方法に関する誤情報、過去に行われた選挙の結果に対する虚偽の主張</li>
+                    </ul>
+                    <li>人種、国籍、病気、マイノリティ、性別、性自認、信仰などに対する差別的な主張に基づくコンテンツ</li>
+                    <li>他人の権利を侵害するコンテンツ</li>
+                    <li>宗教活動、その他の特定の容認できない目的を持つコンテンツ</li>
+                    <li>有害なコンピュータプログラム、ウイルス、その他の有害なコンテンツ</li>
+                </ul>
+                <p class="mt-2">
+                    これらの禁止事項に違反した場合、並びに運営者が不適切と判断した行為を行った場合、理由の如何に関わらず一切の予告なくアカウントを永久に停止し、今後のサービスの利用を拒否することがあります。
+                </p>
+            </div>
+
+            <p class="text-lg mt-8">Ⅳ. プライバシーポリシー</p>
+            <p class="mt-2">
+                本サービスは、本サービスの利用者（以下、ユーザー）のプライバシーを尊重し、ユーザーの個人情報を適切に取り扱います。<br>
+                本サービスは、ユーザーの以下の情報を取得、保管することがあります。以後、これらを「ユーザーデータ」といいます。
+            </p>
+            <ul class="mt-2 ml-8 list-disc">
+                <li>ユーザーのIPアドレス、ブラウザが送信するヘッダー、Cookie情報</li>
+                <li>ユーザー登録時、認証プロパイダー（Googleなど）からOAuth2経由で取得したユーザー情報（アカウントの固有識別子、メールアドレス、名前、プロフィール画像など）</li>
+                <li>ユーザーがサービスを利用するにあたって明示的に提供した情報（フィードの購読データなど）</li>
+            </ul>
+
+            <p class="mt-4">
+                本サービスは、ユーザーデータを以下の目的で利用します。
+            </p>
+            <ul class="mt-2 ml-8 list-disc">
+                <li>本サービスの提供、運営、維持、保護、改善</li>
+                <li>ユーザーの本サービスの利用状況の把握、分析、統計</li>
+                <li>本サービスの利用規約に違反する行為の防止、調査、対処</li>
+            </ul>
+
+            <p class="mt-4">
+                本サービスは、ユーザーデータを以下のサービスを使用して取得、保管、処理することがります。該当サービスでのユーザーデータの取り扱いについては、各サービスのプライバシーポリシーをご確認ください。
+            </p>
+            <ul class="mt-2 ml-8 list-disc">
+                <li>Google Cloud Platform（Google LLC）, Firebase（Google LLC）</li>
+                <li>Cloudflare（Cloudflare, Inc.）</li>
+                <li>Vercel（Vercel Inc.）</li>
+                <li>OVHcloud (OVH SAS.)</li>
+            </ul>
+
+            <p class="mt-4">
+                本サービスで提供されるウェブサイトでは、以下のサードパーティのリソースを使用しています。これらのサービスにユーザーの機密性の高い情報が送信されることはありませんが、IPアドレスなどの一部のユーザーデータが送信される可能性があります。
+                該当サービスでのユーザーデータの取り扱いについては、各サービスのプライバシーポリシーをご確認ください。
+            </p>
+            <ul class="mt-2 ml-8 list-disc">
+                <li>Google Fonts（Google LLC）</li>
+                <li>jsdelivr（jsdelivr.com）</li>
+            </ul>
+
+            <p class="mt-4">
+                本サービスは、ユーザーの明示的な同意なしにユーザーデータを第三者に提供することはありません。ただし、日本国内の法的機関からの要請に基づく場合はこの限りではありません。
+            </p>
+
+        </div>
+    </div>
+</div>
+
+<style>
+    .font-outfit {
+        font-family: "Outfit", sans-serif;
+    }
+</style>
