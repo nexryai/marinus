dda9ab0a468f6e8fb1c3766a4c392029a683c048
nexryai <nexryai@sda1.net>
2025-01-12 11:39:39 +0900

    Security: HTMLレスポンスのContent-Typeでcharsetを明示するように XSS対策

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -10,5 +10,10 @@ export const handle: Handle = async ({ event, resolve }) => {
         return elysiaHook(request);
     }
 
-    return resolve(event);
+    const response = await resolve(event);
+    if (response.headers.get("Content-Type") == "text/html") {
+        response.headers.set("Content-Type", "text/html; charset=utf-8");
+    }
+
+    return response;
 };
